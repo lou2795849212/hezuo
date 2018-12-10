463a7fb19be42be4cc1418d411704ed58393f889
lou2495849212 <2795849212@qq.com>
2018-12-10 16:16:03 +0800

    sssss

---
+++ src/main/java/sdsd.java
@@ -0,0 +1,23 @@
+import java.io.Serializable;
+
+public class sdsd implements Serializable{
+    /*ssssssssssssssssssssssssssss*/
+    private String name;
+    private String pwd;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setPwd(String pwd) {
+        this.pwd = pwd;
+    }
+
+    public String getPwd() {
+        return pwd;
+    }
+}
